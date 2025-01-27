af573472aa84d1e10e7bb0dd2ab8e7273a9f0f19
Andy Price <andyprice720@gmail.com>
2025-01-27 22:03:42 +0000

    Feat: implement Ship class with hit() and isSunk() methods
    - Add Ship class with properties: length, hits, and sunk
    - Implement hit() method to increment hits
    - Implement isSunk() method to check if ship is sunk
    - Add unit tests for Ship class to verify functionality

---
+++ src/ship.js
@@ -0,0 +1,21 @@
+// ship.js
+
+class Ship {
+	constructor(length) {
+		this.length = length;
+		this.hits = 0;
+		this.sunk = false;
+	}
+
+	hit() {
+		this.hits++;
+	}
+
+	isSunk() {
+		if (this.hits >= this.length) {
+			return true;
+		}
+	}
+}
+
+module.exports = Ship;
---
+++ tests/ship.test.js
@@ -0,0 +1,35 @@
+// ship.test.js
+
+const Ship = require("../src/ship.js");
+
+describe("Ship", () => {
+	let ship;
+
+	beforeEach(() => {
+		ship = new Ship(3);
+	});
+
+	it("Initializes with the correct properties", () => {
+		expect(ship.length).toBe(3);
+		expect(ship.hits).toBe(0);
+		expect(ship.sunk).toBe(false);
+	});
+
+	it("hit() increments hit", () => {
+		ship.hit();
+		expect(ship.hits).toBe(1);
+	});
+
+	it("isSunk() returns false when hits < length", () => {
+		ship.hit();
+		ship.hit();
+		expect(ship.sunk).toBe(false);
+	});
+
+	it("isSunk() returns true when hits >= length", () => {
+		ship.hit();
+		ship.hit();
+		ship.hit();
+		expect(ship.isSunk()).toBe(true);
+	});
+});
